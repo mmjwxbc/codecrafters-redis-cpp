715efd017f8bd54ed0ddc57c1cc2ffd43d548cc0
mmjwxbc <2891002167@qq.com>
2025-07-11 22:20:27 +0800

    RDB file config test

--- src/Redis.hpp
+++ src/Redis.hpp
@@ -84,7 +84,7 @@ public:
 
         std::string command = items.front().strVal;
         std::transform(command.begin(), command.end(), command.begin(), ::tolower);
-
+        std::cout << command << std::endl;
         if (command == "echo") {
             items.erase(items.begin());
             sendCommand(items, client_fd);
