20212d13057d1366ff550ba88c0323fe9f2f2b6e
mmjwxbc <2891002167@qq.com>
2025-07-11 20:44:22 +0800

    echo command test

--- src/Server.cpp
+++ src/Server.cpp
@@ -82,6 +82,10 @@ int main(int argc, char **argv) {
           close(i);
           continue;
         } else {
+            cout << "Type: ARRAY, Size: " << reply.value().elements.size() << endl;
+          for (size_t i = 0; i < reply.value().elements.size(); ++i) {
+              cout << "Element " << i << ": " << reply.value().elements[i].strVal << endl;
+          }
           redis.process_command(reply.value(), i);
         }
       }
