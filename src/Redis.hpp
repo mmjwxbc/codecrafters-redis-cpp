8a2f28133f00b8893f417151e708928f99012494
mmjwxbc <2891002167@qq.com>
2025-07-13 11:25:15 +0800

    Empty RDB transfer

--- src/Redis.hpp
+++ src/Redis.hpp
@@ -96,6 +96,7 @@ public:
             sendCommand({makeArray({makeBulk("REPLCONF"), makeBulk("capa"), makeBulk("psync2")})}, master_fd);
             reply = readReply(master_fd);
             sendCommand({makeArray({makeBulk("PSYNC"), makeBulk("?"), makeBulk("-1")})}, master_fd);
+            reply = readReply(master_fd);
         }
         metadata.insert_or_assign("dir", dir);
         metadata.insert_or_assign("dbfilename", dbfilename);
@@ -235,6 +236,14 @@ public:
             sendCommand({makeString("OK")}, client_fd);
         } else if(command == "psync") {
             sendCommand({makeString("FULLRESYNC " + metadata["master_replid"] + " " + metadata["master_repl_offset"])}, client_fd);
+            const std::string empty_rdb = "\x52\x45\x44\x49\x53\x30\x30\x31\x31\xfa\x09\x72\x65\x64\x69\x73\x2d\x76\x65"
+            "\x72\x05\x37\x2e\x32\x2e\x30\xfa\x0a\x72\x65\x64\x69\x73\x2d\x62\x69\x74\x73"
+            "\xc0\x40\xfa\x05\x63\x74\x69\x6d\x65\xc2\x6d\x08\xbc\x65\xfa\x08\x75\x73\x65"
+            "\x64\x2d\x6d\x65\x6d\xc2\xb0\xc4\x10\x00\xfa\x08\x61\x6f\x66\x2d\x62\x61\x73"
+            "\x65\xc0\x00\xff\xf0\x6e\x3b\xfe\xc0\xff\x5a\xa2";
+            if(send(client_fd, empty_rdb.c_str(), empty_rdb.size(), 0) != empty_rdb.size()) {
+                throw std::runtime_error("send RDB failed");
+            }
         }
     }
 
