097fa360df8ff068c0b397c2b8811320a8ce7bee
mmjwxbc <2891002167@qq.com>
2025-07-14 16:14:58 +0800

    ACKS with commands debug

--- src/util.cpp
+++ src/util.cpp
@@ -1,5 +1,5 @@
 #include <string>
-
+#include <iostream>
 bool matchPattern(const std::string &pattern, const std::string &text) {
     size_t p = 0, t = 0, star = std::string::npos, match = 0;
 
@@ -21,12 +21,12 @@ bool matchPattern(const std::string &pattern, const std::string &text) {
     return p == pattern.size();
 }
 
-std::string escapeCRLF(const std::string& input) {
+void escapeCRLF(const std::string& input) {
     std::string out;
     for (char c : input) {
         if (c == '\r') out += "\\r";
         else if (c == '\n') out += "\\n";
         else out += c;
     }
-    return out;
+    std::cout << out << std::endl;
 }
