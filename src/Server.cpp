28b9e2dc8333f9375a0c37cb9bef4fa3758fe63b
mmjwxbc <2891002167@qq.com>
2025-07-18 16:13:11 +0800

    ack with command full revision about Protocol RedisInputStream RedisReply

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,17 +1,17 @@
-#include <cstddef>
-#include <iostream>
-#include <cstdlib>
-#include <cstdio>
-#include <cstring>
-#include <string>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <sys/select.h>
 #include "Protocol.hpp"
 #include "Redis.hpp"
+#include <arpa/inet.h>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <netdb.h>
+#include <string>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <vector>
 
 using namespace std;
@@ -24,16 +24,16 @@ int main(int argc, char **argv) {
   int port = -1;
   for (int i = 1; i < argc - 1; ++i) {
     string key = argv[i];
-    if (key == "--dir") {  // key starts with --
-        dir = argv[i + 1];
-        ++i; // Skip value
-    } else if(key == "--dbfilename") {
+    if (key == "--dir") { // key starts with --
+      dir = argv[i + 1];
+      ++i; // Skip value
+    } else if (key == "--dbfilename") {
       dbfiliname = argv[i + 1];
       ++i;
-    } else if(key == "--port") {
+    } else if (key == "--port") {
       port = stoi(argv[i + 1]);
       ++i;
-    } else if(key == "--replicaof") {
+    } else if (key == "--replicaof") {
       replicaof = argv[i + 1];
       ++i;
     }
@@ -47,40 +47,40 @@ int main(int argc, char **argv) {
   FD_SET(server_fd, &fdset);
   int max_fd = server_fd;
   int master_fd = redis.master_fd();
-  if(master_fd != -1) {
-    if(master_fd > max_fd) {
+  if (master_fd != -1) {
+    if (master_fd > max_fd) {
       max_fd = master_fd;
     }
     FD_SET(master_fd, &fdset);
   }
-  while(true) {
+  while (true) {
     fd_set tmp = fdset;
     int ret = select(max_fd + 1, &tmp, NULL, NULL, NULL);
-    if(FD_ISSET(server_fd, &tmp)) {
+    if (FD_ISSET(server_fd, &tmp)) {
       int cfd = accept(server_fd, NULL, NULL);
       FD_SET(cfd, &fdset);
       max_fd = cfd > max_fd ? cfd : max_fd;
     }
-    for(int i = 0; i < max_fd + 1; i++) {
-      if(i != server_fd && FD_ISSET(i, &tmp)) {
-        // while (true) {
-          bool is_close = false;
+    for (int i = 0; i < max_fd + 1; i++) {
+      if (i != server_fd && FD_ISSET(i, &tmp)) {
+        bool is_close = false;
+        while (true) {
           optional<RedisReply> reply = redis.readOneReply(i, is_close);
 
-          if(reply.has_value()){
+          if (reply.has_value()) {
             redis.process_command({reply.value()}, i);
           }
-          if(is_close) { 
+          if (is_close) {
             FD_CLR(i, &fdset);
             close(i);
             break;
-          // redis.process_command({reply}, i);
-          // 只要buffer里还有未处理数据就继续处理
-          // if (!redis.buffer_has_more_data(i)) break;
+          }
+          if (reply.has_value() && !redis.buffer_has_more_data(i)) {
+            break;
+          }
         }
       }
     }
-  }
 
-  return 0;
-}
+    return 0;
+  }
