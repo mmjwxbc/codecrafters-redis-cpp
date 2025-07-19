95c4c51689f05fdee9e22ba303da060a4812a672
mmjwxbc <2891002167@qq.com>
2025-07-19 23:09:48 +0800

    Validating entry IDs #hq8

--- src/Redis.hpp
+++ src/Redis.hpp
@@ -476,12 +476,11 @@ public:
                 if(kvs[cur_db].count(stream_key) != 0) {
                     throw std::runtime_error("stream key already exists in kvs");
                 }
+                if(sequence == "0" && timestamp == "0") {
+                    sendReply({makeError("ERR The ID specified in XADD must be greater than 0-0")}, client_fd);
+                }
                 if(streams.count(stream_key) == 0) {
                     streams[stream_key] = std::vector<RedisStreamEntry>();
-                    if(sequence == "0") {
-                        sendReply({makeError("ERR The ID specified in XADD must be greater than 0-0")}, client_fd);
-                        goto end;
-                    }
                 } else {
                     auto last_id = streams[stream_key].back().id;
                     std::string::size_type pos = last_id.find('-');
