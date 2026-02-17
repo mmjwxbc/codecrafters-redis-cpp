ceace8ae857c43de04bc6b83dc2ea01f720e6a7c
mmjwxbc <2891002167@qq.com>
2026-02-17 11:41:12 +0800

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -19,21 +19,30 @@
 
 using namespace std;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   string dir, dbfiliname, replicaof;
   int port = -1;
-  for (int i = 1; i < argc - 1; ++i) {
+  for (int i = 1; i < argc - 1; ++i)
+  {
     string key = argv[i];
-    if (key == "--dir") {
+    if (key == "--dir")
+    {
       dir = argv[++i];
-    } else if (key == "--dbfilename") {
+    }
+    else if (key == "--dbfilename")
+    {
       dbfiliname = argv[++i];
-    } else if (key == "--port") {
+    }
+    else if (key == "--port")
+    {
       port = stoi(argv[++i]);
-    } else if (key == "--replicaof") {
+    }
+    else if (key == "--replicaof")
+    {
       replicaof = argv[++i];
     }
   }
@@ -45,7 +54,8 @@ int main(int argc, char **argv) {
   const int server_fd = redis.server_fd();
 
   int epoll_fd = epoll_create1(0);
-  if (epoll_fd == -1) {
+  if (epoll_fd == -1)
+  {
     perror("epoll_create1");
     exit(EXIT_FAILURE);
   }
@@ -57,13 +67,18 @@ int main(int argc, char **argv) {
   redis.set_epoll_fd(epoll_fd);
 
   int master_fd = redis.master_fd();
-  if (master_fd != -1) {
+  if (master_fd != -1)
+  {
     vector<RedisReply> master_reply;
-    while (true) {
+    while (true)
+    {
       optional<RedisReply> reply = redis.readOneReply(master_fd);
-      if (reply.has_value()) {
+      if (reply.has_value())
+      {
         master_reply.emplace_back(reply.value());
-      } else {
+      }
+      else
+      {
         break;
       }
     }
@@ -79,63 +94,82 @@ int main(int argc, char **argv) {
   const int MAX_EVENTS = 64;
   epoll_event events[MAX_EVENTS];
 
-  while (true) {
+  while (true)
+  {
     int timeout = redis.process_blpop_timeout_event();
     int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, timeout);
-    if (nfds == -1) {
+    if (nfds == -1)
+    {
       perror("epoll_wait");
       break;
     }
 
-    for (int i = 0; i < nfds; ++i) {
+    for (int i = 0; i < nfds; ++i)
+    {
       int fd = events[i].data.fd;
       int wait_timerfd = redis.wait_event_timer_fd();
       int xread_block_timerfd = redis.xread_block_event_timer_fd();
-      if (fd == server_fd) {
+      if (fd == server_fd)
+      {
         int cfd = accept(server_fd, NULL, NULL);
         // set_non_blocking(cfd);
-        if (cfd == -1) {
+        if (cfd == -1)
+        {
           perror("accept");
           continue;
         }
-
+        redis.add_user(cfd, "default");
         epoll_event client_ev;
         client_ev.events = EPOLLIN;
         client_ev.data.fd = cfd;
         epoll_ctl(epoll_fd, EPOLL_CTL_ADD, cfd, &client_ev);
-      } else if (fd == wait_timerfd) {
+      }
+      else if (fd == wait_timerfd)
+      {
         RedisWaitEvent *timer_event = redis.get_wait_timer_event();
-        if (timer_event != nullptr) {
+        if (timer_event != nullptr)
+        {
           cout << "blpop block timeout send nil" << endl;
           timer_event->on_finish(timer_event->client_fd);
           redis.clear_wait_timer_event();
         }
         epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL);
         close(fd);
-      } else if (fd == xread_block_timerfd) {
+      }
+      else if (fd == xread_block_timerfd)
+      {
         RedisXreadBlockEvent *timer_event = redis.get_xread_block_timer_event();
-        if (timer_event != nullptr) {
+        if (timer_event != nullptr)
+        {
           cout << "xread block timeout send nil" << endl;
           timer_event->on_finish(timer_event->client_fd);
           redis.clear_xread_block_timer_event();
         }
         epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL);
         close(fd);
-      } else {
+      }
+      else
+      {
         bool is_close = false;
         vector<RedisReply> replies;
 
         redis.recv_data(fd, is_close);
-        while (true) {
+        while (true)
+        {
           optional<RedisReply> reply = redis.readOneReply(fd);
-          if (reply.has_value()) {
+          if (reply.has_value())
+          {
             replies.emplace_back(reply.value());
-          } else {
+          }
+          else
+          {
             break;
           }
         }
         redis.process_request(replies, fd);
-        if (is_close) {
+        if (is_close)
+        {
+          redis.del_user(fd);
           epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL);
           close(fd);
         }
